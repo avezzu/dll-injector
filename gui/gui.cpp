fc0985f6c99b6d26f09b25952859ae51dda1c9e8
avezzu <avezzu@ethz.ch>
2023-11-07 20:27:48 +0100

    removed unused function

--- gui/gui.cpp
+++ gui/gui.cpp
@@ -267,15 +267,6 @@ void InfosPane(const char *vFilter, IGFDUserDatas vUserDatas, bool *vCantContinu
 		*vCantContinue = canValidateDialog;
 }
 
-std::string ReplaceAll(std::string str, const std::string& from, const std::string& to) {
-    size_t start_pos = 0;
-    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
-        str.replace(start_pos, from.length(), to);
-        start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
-    }
-    return str;
-}
-
 void drawGui()
 {
 	// open Dialog with Pane
